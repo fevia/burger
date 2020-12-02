064148ca8f1ebc39c5579ad98c99c15d3bce6656
Flory Ann Evia <floryann.evia@gmail.com>
2020-12-02 11:10:59 -0600

    database setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Southwest Pepperjack Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Parm Burger');
+INSERT INTO burgers (burger_name) VALUES ('Salmon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
